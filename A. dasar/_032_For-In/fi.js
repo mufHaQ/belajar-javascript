7ff0a2a93bd12a0991137f83c3b685528e309d47
mufHaQ <masbrowmess33@gmail.com>
2021-07-05 00:56:59 +0700

    update

---
+++ A. dasar/_032_For-In/fi.js
@@ -0,0 +1,34 @@
+console.log("===For In===")
+
+
+// ================================================================================
+// For In:
+// * For In merupakan perulangan for yang digunakan untuk mengiterasi seluruh data property di dalam Object ada index di Array
+// * Walaupun For In bisa digunakan untuk Array, namun tidak direkomendasikan untuk Array, karena biasanya kita jarang sekali butuh data index untuk Array
+// * Di Object akan mereturn nama propertynya, di Array akan mereturn nomor indexnya
+// ================================================================================
+
+
+// ================================================================================
+// Kode For In:
+
+// const person = {
+//     firstName: "Dliyaulhaq",
+//     lastName: "Mufliansyah"
+// }
+
+// for (const property in person) {
+//     console.info(`Property ${property}: ${person[property]}`) // Tidak bisa menggunakan person.property karena mereturn string
+// }
+// ================================================================================
+
+
+// ================================================================================
+// Contoh pada Array:
+
+const nama = ["Dliyaulhaq", "Mufliansyah"]
+
+for (const index in nama) {
+    console.info(`Index ke-${index}: ${nama[index]}`)
+}
+// ================================================================================
---
+++ A. dasar/_033_For-Of/fo.js
@@ -0,0 +1,15 @@
+console.log("===For Of===")
+
+
+// ================================================================================
+// For Of:
+// * Jika For In digunakan untuk melakukan iterasi property/index, maka For Of digunakan untuk melakukan iterasi terhadap isi value dari iterable object, seperti Array dan String
+// * For Of tidak bisa digunakan untuk melakukan iterasi pada tipe data Object, karena Object bukanlan iterable
+// ================================================================================
+
+
+// ================================================================================
+// Kode For Of:
+
+
+// ================================================================================
